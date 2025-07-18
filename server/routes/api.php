fee8f09f8473e712579353db72185f1632e8a3c1
Tshims <manassetshims@gmail.com>
2025-07-18 23:23:46 +0100

    configuration des routes pour les methodes transfer, transactions, wallet/purchase, wallet/transferPlan

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,4 +12,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/wallet/balance', [WalletController::class, 'getBalance']);
     Route::post('/wallet/recharge', [WalletController::class, 'recharge']);
+    Route::post('/transfer', [WalletController::class, 'transfer']);
+    Route::get('/transactions', [WalletController::class, 'getTransactions']);
+    Route::post('/wallet/purchase', [WalletController::class, 'purchasePlan']);
+    Route::post('/wallet/transferPlan', [WalletController::class, 'transferPlanPurcharge']);
 });
