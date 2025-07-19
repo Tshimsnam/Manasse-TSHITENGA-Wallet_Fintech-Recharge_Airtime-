b8ff3de915c547a66be442ef494e0828e0051a19
Tshims <manassetshims@gmail.com>
2025-07-19 01:13:52 +0100

    mise en place des test pour la fonction transfer(argent)

---
+++ server/tests/Feature/TransferTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TransferTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    use RefreshDatabase;
+
+    public function test_user_can_transfer_money()
+    {
+        // on Cree deux utilisateurs
+        $sender = User::factory()->create(['balance' => 10000]);
+        $receiver = User::factory()->create(['balance' => 2000, 'phone' => '+243999888777']);
+
+        //Authentifier le sender
+        $this->actingAs($sender);
+
+        // Envoyer une requête POST
+        $response = $this->postJson('/api/transfer', [
+            'receiver_phone' => $receiver->phone,
+            'amount' => 3000,
+        ]);
+
+        //Assertions
+        $response->assertStatus(200);
+        $response->assertJson(['message' => "vous venez d'effectué un Transfert de3000au numéro+243999888777votre solde actuel est de7000"
+]);
+        //Vérifier que les balances ont été mises à jour
+        $this->assertEquals(7000, $sender->fresh()->balance);
+        $this->assertEquals(5000, $receiver->fresh()->balance);
+
+        //Vérifier que la transaction a été créée
+        $this->assertDatabaseHas('transactions', [
+            'user_id_from' => $sender->id,
+            'user_id_to' => $receiver->id,
+            'type' => 'transfer',
+            'amount' => 3000,
+        ]);
+    }
+}
